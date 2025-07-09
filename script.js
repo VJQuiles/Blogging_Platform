c9468c6571f0dbedf725522a621d3ee3c1a0e645
Vince Quiles <vjquiles@msn.com>
2025-07-09 18:39:47 -0400

    removed debugging statements

--- script.js
+++ script.js
@@ -14,13 +14,11 @@ let postDisplay = document.getElementById('blog-post-list')
 let postId = 1
 
 window.addEventListener('load', () => {
-    //console.log('something is happening on page load')
 
     let savedBlogList
 
     try {
         savedBlogList = JSON.parse(localStorage.getItem('postList'))
-        //console.log(localStorage.getItem('postList'))
     } catch (e) {
         console.error('Error parsing blog post from localStorage:', e)
         savedBlogList = null
@@ -33,17 +31,18 @@ window.addEventListener('load', () => {
 })
 
 postTitle.addEventListener('blur', () => {
+
     if (postTitle.validity.valueMissing) {
         postTitle.setCustomValidity('Blank titles not accepted, please enter a title of at least 10 characters.')
     }
     else {
         postTitle.setCustomValidity('')
     }
-    //console.log('blur is happening')
     postTitleError.textContent = postTitle.validationMessage
 })
 
 postContent.addEventListener('blur', () => {
+
     if (postContent.validity.valueMissing) {
         postContent.setCustomValidity('You gotta write soemthing, if we wanted to see nothing we wouldve jumped on X 🤷🏼‍♂️.')
     }
@@ -55,6 +54,7 @@ postContent.addEventListener('blur', () => {
 
 
 newPostForm.addEventListener('submit', (e) => {
+
     e.preventDefault()
 
     if (!postTitle.validity.valid) {
@@ -76,11 +76,8 @@ newPostForm.addEventListener('submit', (e) => {
     }
 
     postList.push(postItem)
-    // console.log(postItem)
-    // console.log(postList)
 
     localStorage.setItem('postList', JSON.stringify(postList))
-    //console.log(localStorage.getItem('postList'))
 
     postId++
     newPostForm.reset()
@@ -88,7 +85,7 @@ newPostForm.addEventListener('submit', (e) => {
 })
 
 function renderPostList() {
-    //console.log(postList)
+
     postDisplay.innerHTML = ''
     postList.forEach((post) => {
         const postDisplayItem = document.createElement('li')
@@ -100,17 +97,13 @@ function renderPostList() {
             <button class='edit-post'>Edit Post</button>`
         postDisplay.appendChild(postDisplayItem)
     })
-    //console.log(postId)
 }
 
 postDisplay.addEventListener('click', (e) => {
 
-    //console.log(e.target)
-
     if (e.target.matches('.delete-post')) {
         const postToDelete = e.target.closest('li')
         const postIdToDelete = Number(postToDelete.dataset.id)
-        //console.log(typeof (postToDelete.dataset.id))
         const indexToDelete = postList.findIndex((post) => post.postItemId === postIdToDelete)
         postList.splice(indexToDelete, 1)
         localStorage.setItem('postList', JSON.stringify(postList))
@@ -125,17 +118,14 @@ postDisplay.addEventListener('click', (e) => {
         }
         const postIdToEdit = Number(postToEdit.dataset.id)
         const indexToEdit = postList.findIndex((post) => post.postItemId === postIdToEdit)
-        //console.log(postList[indexToEdit])
         if (indexToEdit === -1) return
 
         const editedContent = postToEdit.querySelector('textarea')
 
         if (editedContent) {
             postList[indexToEdit].postItemContent = editedContent.value
-            //console.log('post has been edited')
             localStorage.setItem('postList', JSON.stringify(postList))
             renderPostList()
-            //console.log('you should see the post with the saved edited content')
             return
         }
         else {
